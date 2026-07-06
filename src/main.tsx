35809084494c548c85641eebe50562036c730ea3
Karan Dixit <karandixit016@gmail.com>
2026-07-06 15:57:36 +0530

    Fix React Router basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./context/CartContext.tsx";
 
-  import { createRoot } from 'react-dom/client'
-  import './index.css'
-  import App from './App.tsx'
-  import {BrowserRouter} from 'react-router-dom'
-  import { CartProvider } from './context/CartContext.tsx'
-
-  createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/GreenCart-Grocery-Store">
     <CartProvider>
       <App />
-  
     </CartProvider>
-    </BrowserRouter>,
-  )
+  </BrowserRouter>
+);
