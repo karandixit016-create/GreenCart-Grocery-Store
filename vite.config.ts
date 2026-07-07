bf57a982f1cbb9c86635079b6916bf2be19979d3
Karan Dixit <karandixit016@gmail.com>
2026-07-07 23:07:24 +0530

    Configure base for GitHub and Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/GreenCart-Grocery-Store/",
+  base:
+    process.env.GITHUB_ACTIONS === "true"
+      ? "/GreenCart-Grocery-Store/"
+      : "/",
   plugins: [react(), tailwindcss()],
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   base: "/GreenCart-Grocery-Store/",
+//   plugins: [react(), tailwindcss()],
+// });
