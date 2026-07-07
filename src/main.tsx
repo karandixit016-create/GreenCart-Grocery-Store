cb8da1459b84bc37ea02d0de155ce92c03ff9790
Karan Dixit <karandixit016@gmail.com>
2026-07-07 22:52:07 +0530

    Use HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import { CartProvider } from "./context/CartContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/GreenCart-Grocery-Store">
+  < HashRouter >
     <CartProvider>
       <App />
     </CartProvider>
-  </BrowserRouter>
+  </ HashRouter>
 );
